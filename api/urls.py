c8c5e5e9919ed0086efc469454d26eb80bb751cd
Michal Czaplinski <mmczaplinski@gmail.com>
2015-09-23 00:53:15 +0200

    fixes the urls.py

--- api/urls.py
+++ api/urls.py
@@ -11,5 +11,5 @@ router.register(r'bookmarks', BookmarkViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^auth/', csrf_exempt(include('rest_auth.urls')))
+    url(r'^auth/', include('rest_auth.urls'))
 ]
